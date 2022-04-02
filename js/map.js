2955e82c16fae898ff5ec17b7bc5489907834db4
Remelia Shirlley <83123538+remeliashirlley@users.noreply.github.com>
2022-04-02 21:23:18 +0800

    5.0

--- js/map.js
+++ js/map.js
@@ -1,46 +1,7 @@
-// mapboxgl.accessToken = 'pk.eyJ1IjoicmVtZWxpYXNoaXJsbGV5IiwiYSI6ImNsMWhodDY3NjBjcmszaXB3OTMzemtkbXYifQ.pJeHvUL0LGZGu3YSeVDPjA';
-// const map = new mapboxgl.Map({
-// container: 'map', // container ID
-// style: 'mapbox://styles/mapbox/streets-v11', // style URL
-// center: [-74.5, 40], // starting position [lng, lat]
-// zoom: 9 // starting zoom
-// });
-
-
-let marker;
-let placeName;
-let popup;
 mapboxgl.accessToken = 'pk.eyJ1IjoicmVtZWxpYXNoaXJsbGV5IiwiYSI6ImNsMWhodDY3NjBjcmszaXB3OTMzemtkbXYifQ.pJeHvUL0LGZGu3YSeVDPjA';
-let client = new MapboxClient('pk.eyJ1IjoicmVtZWxpYXNoaXJsbGV5IiwiYSI6ImNsMWhodDY3NjBjcmszaXB3OTMzemtkbXYifQ.pJeHvUL0LGZGu3YSeVDPjA');
-let map = new mapboxgl.Map({
-    // The container property links the div that we defined above to our incoming map
-    container: 'map',
-    // The style property allows us to use our fancy custom style
-    style: 'mapbox://styles/mapbox/streets-v11'
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/remeliashirlley/cl1hi0m82001k15rjscjy23l1', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9 // starting zoom
 });
-
-// The load event listener ensures that we've loaded our map to the page before we even think about markers
-map.on('load', () => {
-    map.on('click', (e) => {
-        addMarker(map.unproject(e.point))
-    })
-})
-
-function addMarker(coords) {
-    // We remove the marker and popup if there is already a marker on our map
-    if (marker) {
-        marker.remove();
-        popup.remove();
-    }
-    // Here we query the Mapbox Geocoder API for a place name, and add our marker and popup to the map once we receive a response
-    client.geocodeReverse({ latitude: coords.lat, longitude: coords.lng }, (err, res) => {
-        placeName = res.features[0].place_name;
-        marker = new mapboxgl.Marker()
-            .setLngLat(coords)
-            .addTo(map);
-        popup = new mapboxgl.Popup({ closeButton: false, anchor: 'top' })
-            .setLngLat(coords)
-            .setHTML(`<p>${placeName}</p>`)
-            .addTo(map);
-    });
-}
