84726351e35af9737b2a04a3dd943de117112a6b
Remelia Shirlley <83123538+remeliashirlley@users.noreply.github.com>
2022-04-04 13:17:02 +0800

    Update js

--- js/map.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicmVtZWxpYXNoaXJsbGV5IiwiYSI6ImNsMWhodDY3NjBjcmszaXB3OTMzemtkbXYifQ.pJeHvUL0LGZGu3YSeVDPjA';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/remeliashirlley/cl1hi0m82001k15rjscjy23l1', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
-});
